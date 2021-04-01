330ef287e7d6ed302b8f6c4e25d8eb30e2311f7c
Georgios Karamanis <georgios.karamanis@me.com>
2021-04-01 07:00:17 +0200

    Initial commit

---
+++ day-1-part-to-whole/day-1-part-to-whole.R
@@ -0,0 +1,26 @@
+library(waffle)
+library(dplyr)
+
+day_1 <- data.frame(day = 1:30, done = c(TRUE, rep(FALSE, 29)))
+
+
+ggplot(day_1) +
+  geom_pictogram(aes(color = done, label = done, values = 1),
+                 family = "Font Awesome 5 Free Solid", size = 10,
+                 flip = TRUE, n_rows = 5) +
+  scale_label_pictogram(
+    name = NULL,
+    values = c("times-circle", "check-circle"),
+    labels = c(TRUE, FALSE)
+  ) +
+  scale_color_manual(values = c("#8023EB", "#58C1AA")) +
+  labs(title = "Plots I've Done for #30DayChartChallenge") +
+  theme_void() +
+  theme(
+    legend.position = "none",
+    plot.title = element_text(hjust = 0.5, family = "American Typewriter Bold", margin = margin(10, 0, 20, 0), size = 14, color = "#eb5223"),
+    plot.margin = margin(20, 20, 10, 20),
+    plot.background = element_rect(fill = "grey97", color = NA)
+  ) +
+  ggsave(here::here("day-1-part-to-whole/day-1-part-to-whole.png"), dpi = 320, width = 5, height = 5)
+
