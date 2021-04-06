8c150d7e1af760d08cf960727fec76e4b7353177
Georgios Karamanis <georgios@karaman.is>
2021-04-06 17:24:34 +0200

    add day 6

---
+++ 2021/day-6-experimental/day-6-experimental.R
@@ -0,0 +1,39 @@
+library(ggplot2)
+library(dplyr)
+library(rtweet)
+library(lubridate)
+library(sonify)
+library(tuneR)
+
+tweets_30 <- search_tweets("#30daychartchallenge", n = 10000, fromDate = "202104010001")
+
+tweets_music <- tweets_30 %>% 
+  mutate(
+    day = day(created_at),
+    hour = hour(created_at)
+    ) %>% 
+  filter(day != 31) %>% 
+  distinct(favorite_count, retweet_count, day, hour) %>% 
+  arrange(day, hour) %>% 
+  complete(day, hour) %>% 
+  replace(is.na(.), 0)
+
+r <- sonify(x = tweets_music$day + tweets_music$hour/25, y = tweets_music$retweet_count, interpolation = "constant", duration = max(tweets_music$day) * 2)
+
+f <- sonify(x = tweets_music$day + tweets_music$hour/25, y = tweets_music$favorite_count, interpolation = "constant", duration = max(tweets_music$day) * 2)
+
+writeWave(r, here::here("2021", "day-6-experimental", "r.wav"))
+writeWave(f, here::here("2021", "day-6-experimental", "f.wav"))
+
+
+ggplot() +
+  geom_text(aes(0, 0, label = "retweets"), color = "grey20", size = 20, family = "Publico Headline Black Italic") +
+  theme_void() +
+  theme(plot.background = element_rect(fill = "orange", color = NA)) +
+  ggsave(here::here("2021", "day-6-experimental", "r.png"))
+
+ggplot() +
+  geom_text(aes(0, 0, label = "favorites"), color = "grey97", size = 20, family = "Publico Headline Black Italic") +
+  theme_void() +
+  theme(plot.background = element_rect(fill = "purple", color = NA)) +
+  ggsave(here::here("2021", "day-6-experimental", "f.png"))
