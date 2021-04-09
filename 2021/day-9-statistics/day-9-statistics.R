e6d93e8f07b14623cca4b1491b55f68a2900cdd5
Georgios Karamanis <georgios.karamanis@me.com>
2021-04-09 19:46:07 +0200

    add day 9

---
+++ 2021/day-9-statistics/day-9-statistics.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(lubridate)
+
+tweets_w1_raw <- read_csv("2021/data/tweets_w1.csv")
+  
+tweets_w1 <- tweets_w1_raw %>% 
+  filter(is_retweet == FALSE) %>%
+  select(status_id, text, hashtags, created_at, lang, day)
+
+tweet_stats <- tweets_w1 %>% 
+  mutate(
+    hour = hour(created_at),
+    lang = fct_rev(fct_other(lang, keep = c("en", "fr", "es"))),
+    lang = fct_recode(lang, English = "en", French = "fr", Spanish = "es")
+  ) %>% 
+  count(hour, lang)
+
+ggplot(tweet_stats, aes(x = hour, y = lang, size = n, label = n)) +
+  geom_violin(color = NA, fill = "#CBC3E3") +
+  geom_point(shape = 21, fill = "orange") +
+  geom_text(nudge_y = -0.125, family = "Fira Sans Bold", color = colorspace::darken("#CBC3E3", 0.7)) +
+  scale_size_continuous(range = c(1.8, 3.5)) +
+  scale_x_continuous(breaks = seq(0, 22, by = 2), limits = c(0, 23), sec.axis = dup_axis()) +
+  labs(
+    title = "Distribution of languages in tweets tagged with\n#30DayChartChallenge by hour of day",
+    caption = "data: Twitter · graphic: Georgios Karamanis"
+       ) +
+  theme_minimal(base_family = "Fira Sans Bold") +
+  theme(
+    legend.position = "none",
+    axis.title = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y = element_blank(),
+    axis.text.x = element_text(color = "grey56"),
+    axis.text.y = element_text(size = 12),
+    plot.background = element_rect(fill = "grey97", color = NA),
+    plot.margin = margin(20, 20, 10, 20),
+    plot.title.position = "plot",
+    plot.title = element_text(hjust = 0.5, margin = margin(0, 0, 10, 0), lineheight = 1, size = 16),
+    plot.caption = element_text(hjust = 0.5, margin = margin(20, 0, 0, 0), size = 8, color = "grey70")
+  ) +
+  ggsave(here::here("2021/day-9-statistics/day-9-statistics.png"), dpi = 320, width = 7, height = 7)
+
--- README.md
+++ README.md
@@ -6,30 +6,35 @@
 
 Submissions to the [#30DayChartChallenge](https://github.com/Z3tt/30DayChartChallenge_2021)
 
-# Day 1, part-to-whole  
+# Comparisons
+## Day 1, part-to-whole  
 ![](2021/day-1-part-to-whole/day-1-part-to-whole.png)  
 ![](2021/day-1-part-to-whole/day-1-moon-phases.png)
 
-# Day 2, pictogram  
+## Day 2, pictogram  
 ![](2021/day-2-pictogram/day-2-pictogram.png)  
 
-# Day 3, historical
+## Day 3, historical
 ![](2021/day-3-historical/day-3-historical.png)  
 
-# Day 4, magical
+## Day 4, magical
 ![](2021/day-4-magical/day-4-magical.png)  
 
-# Day 5, slope
+## Day 5, slope
 ![](2021/day-5-slope/day-5-slope.png)  
 
-# Day 6, experimental
+## Day 6, experimental
 [video](2021/day-6-experimental/r.mp4)  
 
-# Day 7, physical
+# Distributions
+## Day 7, physical
 ![](2021/day-7-physical/day-7-physical.jpeg)  
 
-# Day 8, animals
+## Day 8, animals
 ![](2021/day-8-animals/day-8-animals.png)  
+
+## Day 9, statistics
+![](2021/day-9-statistics/day-9-statistics.png)  
 
 
   
