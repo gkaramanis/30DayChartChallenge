295c515471c2232cf43aed754e583a0a30b64b85
Georgios Karamanis <georgios.karamanis@me.com>
2025-04-01 08:58:37 +0200

    add script for making gifs

---
+++ gifmaker-30daychartchallenge.R
@@ -0,0 +1,10 @@
+# Search and replace path first!
+
+library(camcorder)
+
+# export gif
+gg_record(dir = "2024/30daychart-temp-29")
+gg_playback(frame_duration = 0.15, image_resize = 1080, name = "2024/30daychart-temp-29/animated.gif", playback = FALSE)
+
+# convert to mp4
+system('/opt/homebrew/bin//ffmpeg -i ./2024/30daychart-temp-29/animated.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -vf tpad=stop_mode=clone:stop_duration=0.1 ./2024/30daychart-temp-29/making-of.mp4')
